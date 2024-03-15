0dc3457afd59b700e8cffed5ce2635ab5c3e85f9
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-03-15 09:41:12 +0530

    Math is added

--- interview.py
+++ interview.py
@@ -63,3 +63,160 @@ for i in range(2, 6):
 # Looping from i = 5 to i = 2
 for i in range(5, 1, -1):
     print(i)
+
+
+
+
+
+    
+Math
+
+# Division is decimal by default
+print(5 / 2)
+
+# Double slash rounds down
+print(5 // 2)
+
+# CAREFUL: most languages round towards 0 by default
+# So negative numbers will round down
+print(-3 // 2)
+
+# A workaround for rounding towards zero
+# is to use decimal division and then convert to int.
+print(int(-3 / 2))
+
+
+# Modding is similar to most languages
+print(10 % 3)
+
+# Except for negative values
+print(-10 % 3)
+
+# To be consistent with other languages modulo
+import math
+from multiprocessing import heap
+print(math.fmod(-10, 3))
+
+# More math helpers
+print(math.floor(3 / 2))
+print(math.ceil(3 / 2))
+print(math.sqrt(2))
+print(math.pow(2, 3))
+
+# Max / Min Int
+float("inf")
+float("-inf")
+
+# Python numbers are infinite so they never overflow
+print(math.pow(2, 200))
+
+# But still less than infinity
+print(math.pow(2, 200) < float("inf"))
+
+Arrays
+
+# Arrays (called lists in python)
+arr = [1, 2, 3]
+print(arr)
+
+# Can be used as a stack
+arr.append(4)
+arr.append(5)
+print(arr)
+
+arr.pop()
+print(arr)
+
+arr.insert(1, 7)
+print(arr)
+
+arr[0] = 0
+arr[3] = 0
+print(arr)
+
+# Initialize arr of size n with default value of 1
+n = 5
+arr = [1] * n
+print(arr)
+print(len(arr))
+
+# Careful: -1 is not out of bounds, it's the last value
+arr = [1, 2, 3]
+print(arr[-1])
+
+# Indexing -2 is the second to last value, etc.
+print(arr[-2])
+
+# Sublists (aka slicing)
+arr = [1, 2, 3, 4]
+print(arr[1:3])
+
+# Similar to for-loop ranges, last index is non-inclusive
+print(arr[0:4])
+
+# But no out of bounds error
+print(arr[0:10])
+
+# Unpacking
+a, b, c = [1, 2, 3]
+print(a, b, c)
+
+# Be careful though
+# a, b = [1, 2, 3]
+
+# Loop through arrays
+nums = [1, 2, 3]
+
+# Using index
+for i in range(len(nums)):
+    print(nums[i])
+
+# Without index
+for n in nums:
+    print(n)
+
+# With index and value
+for i, n in enumerate(nums):
+    print(i, n)
+
+# Loop through multiple arrays simultaneously with unpacking
+nums1 = [1, 3, 5]
+nums2 = [2, 4, 6]
+for n1, n2 in zip(nums1, nums2):
+    print(n1, n2)
+
+# Reverse
+nums = [1, 2, 3]
+nums.reverse()
+print(nums)
+
+# Sorting
+arr = [5, 4, 7, 3, 8]
+arr.sort()
+print(arr)
+
+arr.sort(reverse=True)
+print(arr)
+
+arr = ["bob", "alice", "jane", "doe"]
+arr.sort()
+print(arr)
+
+# Custom sort (by length of string)
+arr.sort(key=lambda x: len(x))
+print(arr)
+
+
+# List comprehension
+arr = [i for i in range(5)]
+print(arr)
+
+# 2-D lists
+arr = [[0] * 4 for i in range(4)]
+print(arr)
+print(arr[0][0], arr[3][3])
+
+# This won't work
+# arr = [[0] * 4] * 4
+
+
